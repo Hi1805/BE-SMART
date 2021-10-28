d5616b5cf7aa91efcab690f8886c85a6d51f3603
Huy Trương Thành <ripker1805@gmail.com>
2021-10-28 13:03:05 +0700

    add api

--- src/index.ts
+++ src/index.ts
@@ -13,36 +13,35 @@ const port = 4000;
 app.get("/api", async (req, res) => {
   try {
     const { year, month } = req.query;
-    const list = (await db.collection("Students").get()).docs;
-    const newList = [];
-    for (const student of list) {
-      const attendances = (
-        await db
-          .collection("Students")
-          .doc(student.id)
-          .collection("attendance")
-          .where("month", "==", `${month}-${year}`)
-          .get()
-      ).docs.map((item) => ({
-        date: item.id,
-        data: item.data(),
-      }));
-      newList.push({
-        student: student.data(),
-        attendances,
-        uid: student.id,
-      });
-    }
-    console.log("================== run midlle 3 ================");
-    return res.status(200).send(list);
+    const list = (await db.collection("Students").get()).docs.map(
+      async (student) => {
+        const attendances = (
+          await db
+            .collection("Students")
+            .doc(student.id)
+            .collection("attendance")
+            .where("month", "==", `${month}-${year}`)
+            .get()
+        ).docs.map((item) => ({
+          date: item.id,
+          data: item.data(),
+        }));
+        return {
+          student: student.data(),
+          attendances,
+          uid: student.id,
+        };
+      }
+    );
+    const responseList = await Promise.all(list);
+    return res.status(200).send(responseList);
   } catch (error) {
     console.log(error);
-
     return res.status(401).send({
       message: error,
     });
   }
 });
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || port, () => {
   console.log("listening on http://localhost:4000/api");
 });
